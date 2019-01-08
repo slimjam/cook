e0963e0ddd0e75494c05d3060e3a61378fd5e560
Andrey <andreyoskriko@gmail.com>
2019-01-08 20:34:14 +0300

    init commit

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     render() {
     return (
       <div className="App">
-          <h1>Users</h1>
+          <h1>init commit</h1>
           <ul>
               {this.state.users.map(user =>
               <li key={user.id}>{user.username}</li>
